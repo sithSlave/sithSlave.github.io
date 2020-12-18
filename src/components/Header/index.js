86ad8cf74ff2a81d3d7098ce841ae38b0c9b90ef
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-12-17 22:21:39 -0700

    added bootstrap, and initialized the app.

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './style.css'
+import 'bootstrap';
+
+function Header() {
+   return (
+      <header>
+         <div className='username'>Josh Miller</div>
+      </header>
+   )
+};
+
+export default Header;
