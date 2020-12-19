da42a7c629d127ab9568165bfaf74404df5cebe9
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-12-19 16:34:03 -0700

    embedded my pdf into the resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import resume from '../../assets/JoshuaMillerResume.pdf'
+import './style.css';
 
 function Resume() {
    return (
       <section>
-         Resume Section
+         <h2>Resume</h2> 
+         <iframe src={resume} title='Josh Miller Resume' type='application/pdf' width='100%' height='100%' />
       </section>
    );
 }
